023837647790b8c867b5427c5efca70e54c5bbae
Silve27 <k.prp0127@gmail.com>
2024-10-27 20:37:35 +0700

    commit

--- app/ui/cart/checkout.tsx
+++ app/ui/cart/checkout.tsx
@@ -109,11 +109,11 @@ export default function Checkout({
         </div>
           <div className="mt-6 flex justify-end gap-4">
             <button className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-                onClick={() => cancelCartItems()}
+                onClick={async () => await cancelCartItems()}
             >
                 Cancel
             </button>
-            <Button onClick={() => checkoutCart()}>
+            <Button onClick={async () => await checkoutCart()}>
                 Checkout
             </Button> 
           </div>
